0ed713c9a660bb477ee4ea5dc3fce01045c8639e
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-03-24 20:17:36 -0300

    Criando documentacao

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sqlite3
 
 app = Flask(__name__)
@@ -64,6 +64,19 @@ def listar_livros():
         }
         livros_formatados.append(dicionario_livros)
     return jsonify(livros_formatados)
+
+
+@app.route('/livros/<int:livro_id>', methods=['DELETE'])
+def deletar_livro(livro_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM livros WHERE id = ?", (livro_id,))
+        conn.commit()
+
+    if cursor.rowcount == 0:
+        return jsonify({"erro": "Livro não encontrado"}), 400
+    
+    return jsonify({"menssagem": "Livro excluído com sucesso"}), 200
 
 
 if __name__ == '__main__':
